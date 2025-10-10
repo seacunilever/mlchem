c8313251f7e6ae8a825d17856343425f71a7a3c8
Leonardo Contreas <leonardo.contreas@unilever.com>
2025-10-10 12:47:03 +0100

    add copyright statement

--- tests/ml/preprocessing/test_feature_transformation.py
+++ tests/ml/preprocessing/test_feature_transformation.py
@@ -1,3 +1,36 @@
+# mlchem - cheminformatics library
+# Copyright © 2025 as Unilever Global IP Limited
+
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted under the terms of the BSD-3 License, provided that the following conditions are met:
+
+#     1. Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#
+#     2. Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in
+#        the documentation and/or other materials provided with the distribution.
+#
+#     3. Neither the name of the copyright holder nor the names of its
+#        contributors may be used to endorse or promote products derived
+#        from this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# You should have received a copy of the BSD-3 License along with mlchem.
+# If not, see https://interoperable-europe.ec.europa.eu/licence/bsd-3-clause-new-or-revised-license .
+# It is the responsibility of mlchem users to familiarise themselves with all dependencies and their associated licenses.
+
 import pytest
 import pandas as pd
 import numpy as np
